6360d9d4ddf59f2ba16372a10acdaf41e2376e1b
Dylan Byeon <dylan@primemas.com>
2025-05-15 09:21:24 +0900

    plugin: add vim-better-whitespace

--- init.lua
+++ init.lua
@@ -254,6 +254,10 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  {
+    'ntpeters/vim-better-whitespace',
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
